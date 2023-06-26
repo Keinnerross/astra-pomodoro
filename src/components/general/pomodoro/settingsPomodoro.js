971984ffbc53624dfad452326edb61b68d84d807
keinnerross <keinnerross@gmail.com>
2023-06-25 21:12:39 -0400

    settingPomoStyles

--- src/components/general/pomodoro/settingsPomodoro.js
+++ src/components/general/pomodoro/settingsPomodoro.js
@@ -14,64 +14,77 @@ const SettingsPomodoro = ({ updateSetting, ifOpen, closeSetting }) => {
   return (
     <div className={ifOpen ? styles.settingMain : styles.hidden}>
       <div className={styles.settingContainer}>
-        <div className={styles.titleCloseSetting}>
-          <p>Time Setting</p>
-          <button onClick={() => closeSetting(false)}>
-            <h2>x</h2>
+        <div className={styles.settingSection}>
+          <div className={styles.titleCloseSetting}>
+            <p>Settings</p>
+            <button onClick={() => closeSetting(false)}>
+              <h2>x</h2>
+            </button>
+          </div>
+          <div className={styles.titleSection}>
+            <h4>Timer</h4>
+          </div>
+          <h4>time (minutes)</h4>
+          <div className={styles.valuePomoSection}>
+            <div className={styles.valuePomos}>
+              <p>Pomodoro</p>
+              <input
+                className={styles.input}
+                type="number"
+                defaultValue={25}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    pomodoro: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className={styles.valuePomos}>
+              <p>Short Break</p>
+              <input
+                defaultValue={5}
+                className={styles.input}
+                type="number"
+                onChange={(e) => {
+                  setInputValues({ ...inputValues, short: e.target.value });
+                }}
+              />
+            </div>
+            <div className={styles.valuePomos}>
+              <p>Long Break</p>
+              <input
+                defaultValue={15}
+                className={styles.input}
+                type="number"
+                onChange={(e) => {
+                  setInputValues({ ...inputValues, long: e.target.value });
+                }}
+              />
+            </div>
+          </div>
+          <div className={styles.titleSection}>
+            <h4>Sounds</h4>
+          </div>
+          <div className={styles.soundSection}>
+            <span>Alarm Sound</span>
+            <select
+              // selectedValue={changeSound}
+              // onValueChange={(value) => changeSound(value)}
+              className={styles.picker}
+            >
+              <option label="Epic 1" value="1" />
+              <option label="Epic 2" value="2" />
+              <option label="Epic 3" value="3" />
+              <option label="Clasic Alarm" value="0" />
+            </select>
+          </div>
+        </div>
+        <div className={styles.footerSettingSection}>
+          <button onClick={() => updateSetting(inputValues)}>
+            <span>Done</span>
           </button>
         </div>
-
-        <div className={styles.valuePomos}>
-          <p>Pomodoro</p>
-          <input
-            className={styles.input}
-            defaultValue={25}
-            onChange={(e) => {
-              setInputValues({
-                ...inputValues,
-                pomodoro: e.target.value,
-              });
-            }}
-          />
-        </div>
-        <div className={styles.valuePomos}>
-          <p>Short Break</p>
-          <input
-            defaultValue={5}
-            className={styles.input}
-            type="number"
-            onChange={(e) => {
-              setInputValues({ ...inputValues, short: e.target.value });
-            }}
-          />
-        </div>
-        <div className={styles.valuePomos}>
-          <p>Long Break</p>
-          <input
-            defaultValue={15}
-            className={styles.input}
-            type="number"
-            onChange={(e) => {
-              setInputValues({ ...inputValues, long: e.target.value });
-            }}
-          />
-        </div>
-        <div className={styles.valuePomos}>
-          <h2>Sound Setting</h2>
-          <select
-            // selectedValue={changeSound}
-            // onValueChange={(value) => changeSound(value)}
-            className={styles.picker}
-          >
-            <option label="Epic 1" value="1" />
-            <option label="Epic 2" value="2" />
-            <option label="Epic 3" value="3" />
-            <option label="Clasic Alarm" value="0" />
-          </select>
-        </div>
-        <button onClick={() => updateSetting(inputValues)}>
-          <h3 style={styles.doneButton}>DONE</h3>
-        </button>
       </div>
     </div>
   );
